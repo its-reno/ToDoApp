15d521dd5630fdb469438ba13f04cf6969c3ce1f
reno <prenomail@gmail.com>
2024-11-03 14:27:17 +0100

    Moved business logic to the TodoList class.

--- src/todolist/Main.java
+++ src/todolist/Main.java
@@ -1,8 +1,6 @@
 package todolist;
 
-import java.time.LocalDate;
 import java.util.Scanner;
-import java.util.UUID;
 
 public class Main {
 
@@ -19,24 +17,24 @@ public class Main {
 
             switch (menuChoice) {
                 case 1:
-                    addNewItem(scanner); // Method to add a new to-do item
+                    todoList.addItem(scanner); // Method to add a new to-do item
                     break;
                 case 2:
-                    showAllItems(); // Method to display all items
+                    todoList.getAllItems(); // Method to display all items
                     break;
 
                 case 3:
-                    markItemAsCompleted(scanner); // Method to mark an item as completed
+                    todoList.markItemAsCompleted(scanner); // Method to mark an item as completed
                     break;
 
                 case 4:
-                    updateTask(scanner); // Method to remove an item by ID
+                    todoList.updateTask(scanner); // Method to modify an item by ID
                     break;
                 case 5:
-                    showPendingItems(); // Method to display only pending items
+                    todoList.getPendingItems(); // Method to display only pending items
                     break;
                 case 6:
-                    showCompletedItems(); // Method to display only completed items
+                    todoList.getCompletedItems(); // Method to display only completed items
                     break;
                 case 0:
                     running = false; // Exit the loop and end the program
@@ -51,23 +49,6 @@ public class Main {
         scanner.close();
     }
 
-
-    private static void updateTask(Scanner scanner) {
-        System.out.println("Enter the ID of the task you want to modify: ");
-        UUID uuid = UUID.fromString(scanner.nextLine());
-        System.out.println("Enter the new title: ");
-        String newTitle = scanner.nextLine();
-        System.out.println("Enter new description: ");
-        String newDescription = scanner.nextLine();
-        System.out.println("Enter new due date: ");
-        String newDueDateString = scanner.nextLine();
-        LocalDate newDueDate = null;
-        if (!newDueDateString.isEmpty())
-            newDueDate = LocalDate.parse(newDueDateString);
-
-        todoList.updateTask(uuid, newTitle, newDescription, newDueDate);
-    }
-
     private static void showMenu() {
         System.out.println("Select what you want to do:");
         System.out.println("1. Add new todo item");
@@ -79,36 +60,4 @@ public class Main {
         System.out.println("0. Quit program");
         System.out.println("=========================");
     }
-
-    private static void showCompletedItems() {
-        todoList.getCompletedItems();
-    }
-
-    private static void showPendingItems() {
-        todoList.getPendingItems();
-    }
-
-    private static void markItemAsCompleted(Scanner scanner) {
-        System.out.println("Enter the ID of the item that's done: ");
-        UUID uuid = UUID.fromString(scanner.nextLine());
-        todoList.markItemAsCompleted(uuid);
-    }
-
-    private static void addNewItem(Scanner scanner) {
-        System.out.println("What's the title?");
-        String title = scanner.nextLine();
-        System.out.println("What's the description");
-        String description = scanner.nextLine();
-        System.out.println("Enter due date:");
-        LocalDate dueDate = LocalDate.parse(scanner.nextLine());
-
-        TodoItem item = new TodoItem(title, description, dueDate);
-
-        todoList.addItem(item);
-    }
-
-    private static void showAllItems() {
-        todoList.getAllItems();
-    }
-
 }
--- src/todolist/TodoList.java
+++ src/todolist/TodoList.java
@@ -3,15 +3,26 @@ package todolist;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class TodoList {
     public List<TodoItem> items = new ArrayList<>();
 
     //add a new item to the list
-    public void addItem(TodoItem item) {
-        items.add(item);
-    }
+    public void addItem(Scanner scanner) {
+            System.out.println("What's the title?");
+            String title = scanner.nextLine();
+            System.out.println("What's the description");
+            String description = scanner.nextLine();
+            System.out.println("Enter due date:");
+            LocalDate dueDate = LocalDate.parse(scanner.nextLine());
+
+            TodoItem item = new TodoItem(title, description, dueDate);
+
+            items.add(item);
+        }
+
 
     public void getAllItems(){
         for (TodoItem t : items) {
@@ -19,7 +30,9 @@ public class TodoList {
         }
     }
 
-    public void markItemAsCompleted(UUID uuid) {
+    public void markItemAsCompleted(Scanner scanner) {
+        System.out.println("Enter the ID of the item that's done: ");
+        UUID uuid = UUID.fromString(scanner.nextLine());
         for (TodoItem t : items) {
             if (t.getId().equals(uuid)) {
                 t.setCompleted();
@@ -44,7 +57,20 @@ public class TodoList {
         }
     }
 
-    public void updateTask(UUID uuid, String newTitle, String newDescription, LocalDate newDueDate) {
+    public void updateTask(Scanner scanner) {
+        System.out.println("Enter the ID of the task you want to modify: ");
+        UUID uuid = UUID.fromString(scanner.nextLine());
+        System.out.println("Enter the new title: ");
+        String newTitle = scanner.nextLine();
+        System.out.println("Enter new description: ");
+        String newDescription = scanner.nextLine();
+        System.out.println("Enter new due date: ");
+        String newDueDateString = scanner.nextLine();
+        LocalDate newDueDate = null;
+        if (!newDueDateString.isEmpty())
+            newDueDate = LocalDate.parse(newDueDateString);
+
+
         for (TodoItem t : items) {
             if (t.getId().equals(uuid)) {
                 if(!newTitle.isEmpty())
